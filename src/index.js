c3c48487ed424eeb1c65d8a9234717d6adcb434a
Jonas Franke <jonasfranke@sdevs.org>
2024-10-17 10:56:25 +0200

    Refactor command structure, add Jest configuration, and update package dependencies

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
-
-// Import required modules
-import { exec } from 'child_process';
 import { argv } from 'process';
 import { help, version } from './commands/index.js';
+import { install } from './commands/install.js';
 
 function main() {
   const command = argv[2];
-
   switch (command) {
     case 'help':
       help();
       break;
     case 'version':
       version();
+      break;
+    case 'install':
+      install(argv.slice(3));
       break;
     default:
       console.log('Unknown command. Use "pacm help" for a list of commands.');
