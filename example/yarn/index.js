31555981864eeea32622caaed1403b7b80f2af0a
Jonas Franke <jonasfranke@sdevs.org>
2024-10-23 11:30:58 +0000

    package managers tests

---
+++ example/yarn/index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const main = (async () => {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    console.log(response.data);
+});
+
+main();
