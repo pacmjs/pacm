003cfcbae845588512252e633343963c418b4ab7
Jonas Franke <jonasfranke@sdevs.org>
2024-10-24 20:25:09 +0200

    Refactor clean command to use logger and exit process

--- commands/clean.js
+++ commands/clean.js
@@ -1,14 +1,24 @@
 import { existsSync, rmSync } from "node:fs";
 import { join } from "node:path";
 import { homedir } from "node:os";
+import logger from "../lib/logger.js";
+import process from "node:process";
 
 export function clean() {
   const cacheDir = join(homedir(), ".pacm-cache");
 
   if (existsSync(cacheDir)) {
     rmSync(cacheDir, { recursive: true, force: true });
-    console.log("Cache cleared successfully.");
+    logger.logSuccess({
+      message: "Cache cleared successfully.",
+      successType: " PACM_CACHE_CLIENT ",
+    });
+    process.exit(0);
   } else {
-    console.log("No cache found to clear.");
+    logger.logError({
+      message: "Cache is already empty.",
+      errorType: " PACM_CACHE_CLIENT ",
+    });
+    process.exit(1);
   }
 }
