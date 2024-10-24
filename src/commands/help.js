4b78d69ad6730fc5f22dd95a2fdf503733ec21db
Jonas Franke <jonasfranke@sdevs.org>
2024-10-24 12:11:44 +0200

    Refactor help command to display all available commands

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,6 +5,16 @@ function displayHelp() {
       Commands:
         help      Display this help message
         version   Show the version of pacm
+        init      Initialize a new pacm project
+        install   Install dependencies
+        add       Add a new dependency
+        remove    Remove a dependency
+        list      List all dependencies
+        update    Update a dependency
+        search    Search for a package
+        link      Link a package
+        unlink    Unlink a package
+        publish   Publish a package to the NPM registry
     `);
 }
 
