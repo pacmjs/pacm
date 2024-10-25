cf374654ff831ccde8a778c6c6414944eb58a2d9
Jonas Franke <jonasfranke@sdevs.org>
2024-10-25 21:06:20 +0200

    Refactor release workflow to update application icon

---
+++ scripts/update-icon.js
@@ -0,0 +1,30 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import process from 'node:process';
+import rcedit from 'rcedit';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const exePath = path.join(__dirname, '../dist/pacm-win.exe');
+const iconPath = path.join(__dirname, '../assets/logo.ico');
+
+rcedit(exePath, {
+  icon: iconPath,
+  'version-string': {
+    CompanyName: 'Buzzr Works',
+    FileDescription: 'PACM',
+    ProductName: 'PACM',
+    LegalCopyright: 'Buzzr Works',
+    LegalTrademarks1: 'Buzzr Works',
+    OriginalFilename: 'pacm-win.exe',
+  },
+  'product-version': '1.0.0',
+  'file-version': '1.0.0'
+}, (err) => {
+  if (err) {
+    console.error(`Error: ${err}`);
+    process.exit(1);
+  }
+  console.log('Icon updated successfully');
+});
