a7df8fb58ce2618589d4b283c1d183b76b7dd81d
Jonas Franke <jonasfranke@sdevs.org>
2024-10-26 18:56:53 +0200

    Refactor fetchPackageMetadata function

--- utils/fetchPackageMetadata.js
+++ utils/fetchPackageMetadata.js
@@ -1,7 +1,6 @@
 import fetch from "node-fetch";
 import { retryOnECONNRESET } from "./retry.js";
 import chalk from "chalk";
-import logger from "../lib/logger.js";
 
 export async function fetchPackageMetadata(
   packageNames,
@@ -10,14 +9,6 @@ export async function fetchPackageMetadata(
   totalPackages,
   isForce,
 ) {
-  if (!Array.isArray(packageNames)) {
-    logger.logError({
-      message: "packageNames should be an array",
-      exit: true,
-      errorType: " PACM_ERROR ",
-    });
-  }
-
   spinner.text = `${isForce ? chalk.bgYellow("FORCE") : ""} [${currentPackageIndex}/${totalPackages}] Fetching metadata for packages: ${packageNames.join(", ")}`;
   const fetchMetadata = async (packageName) => {
     const response = await fetch(`https://registry.npmjs.org/${packageName}`);
