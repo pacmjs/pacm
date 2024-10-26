6f063ac2240ab10895d18a76deded386f09e4eca
Jonas Franke <jonasfranke@sdevs.org>
2024-10-26 21:22:57 +0200

    Refactor package.json to remove unnecessary build scripts

--- scripts/update-icon.js
+++ scripts/update-icon.js
@@ -6,25 +6,29 @@ import rcedit from 'rcedit';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const exePath = path.join(__dirname, '../release/pacm.exe');
+const architectures = ['x64', 'arm64'];
 const iconPath = path.join(__dirname, '../assets/logo.ico');
 
-rcedit(exePath, {
-  icon: iconPath,
-  'version-string': {
-    CompanyName: 'Buzzr Works',
-    FileDescription: 'PACM',
-    ProductName: 'PACM',
-    LegalCopyright: 'Buzzr Works',
-    LegalTrademarks1: 'Buzzr Works',
-    OriginalFilename: 'pacm-win.exe',
-  },
-  'product-version': '1.0.0-alpha.2',
-  'file-version': '1.0.0-alpha.2'
-}, (err) => {
-  if (err) {
-    console.error(`Error: ${err}`);
-    process.exit(1);
-  }
-  console.log('Icon updated successfully');
+architectures.forEach(arch => {
+  const exePath = path.join(__dirname, `../release/pacm-windows-${arch}.exe`);
+
+  rcedit(exePath, {
+    icon: iconPath,
+    'version-string': {
+      CompanyName: 'Buzzr Works',
+      FileDescription: 'PACM',
+      ProductName: 'PACM',
+      LegalCopyright: 'Buzzr Works',
+      LegalTrademarks1: 'Buzzr Works',
+      OriginalFilename: `pacm-windows-${arch}.exe`,
+    },
+    'product-version': '1.0.0-alpha.2',
+    'file-version': '1.0.0-alpha.2'
+  }, (err) => {
+    if (err) {
+      console.error(`Error updating icon for ${arch}: ${err}`);
+      process.exit(1);
+    }
+    console.log(`Icon updated successfully for ${arch}`);
+  });
 });
